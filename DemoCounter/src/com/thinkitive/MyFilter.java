0d97874809281942b01f31a058e9a58da14e7f5f
phadatul <phadatul@gmail.com>
2021-01-27 15:28:41 +0530

    Add files via upload

---
+++ DemoCounter/src/com/thinkitive/MyFilter.java
@@ -0,0 +1,55 @@
+package com.thinkitive;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+@WebFilter("/MyServlet")
+public class MyFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MyFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//EXECUTE before servlet
+		PrintWriter out=response.getWriter();
+		
+		out.println("pre filter......");
+		chain.doFilter(request, response);
+		
+		//Execute after servlet
+		out.println("post filter......");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ DemoCounter/src/com/thinkitive/Two.java
@@ -0,0 +1,49 @@
+package com.thinkitive;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Two
+ */
+@WebServlet("/Two")
+public class Two extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Integer counter = 0;
+
+	public Two() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		counter = (Integer) session.getAttribute("count");
+		if (counter != null) {
+			counter++;
+			session.setAttribute("count", counter);
+		} else {
+			counter = 0;
+			counter++;
+			session.setAttribute("count", counter);
+		}
+		response.getWriter().println("SERLVET TWO VISITOR:" + counter);
+
+	}
+
+}
