bd9051a066a2d808d97bb8ea29bee2022dc5f2db
phadatul <phadatul@gmail.com>
2021-01-27 13:19:12 +0530

    Add files via upload

---
+++ SessionsUsingCookies/src/com/thinkitive/ProfileServlet.java
@@ -0,0 +1,53 @@
+package com.thinkitive;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProfileServlet
+ */
+@WebServlet("/ProfileServlet")
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		
+		Cookie ck[]=request.getCookies();
+		if(ck!=null) {
+		out.println("WELCOME TO YOUR PROFILE---");
+		out.print(ck[0].getValue());
+		//SESSION END or LOG OUT
+		ck[0].setMaxAge(0);
+		response.addCookie(ck[0]);
+		}else {
+			out.println("Please login to view this page");
+		}
+	}
+
+}
